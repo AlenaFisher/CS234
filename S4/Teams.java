76301534472404f1bc07305bc423aecd51a0238a
AlenaFisher <123783995+AlenaFisher@users.noreply.github.com>
2024-05-08 22:55:19 -0600

    Add files via upload

---
+++ S4/Teams.java
@@ -0,0 +1,74 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Teams {
+    // Creating the linked lists
+    private LinkedList<Team> teams;
+    private LinkedList<Team> team;
+
+    public Teams() {
+        /**
+         * This constructor initializes two lists: one to store information for individual teams, and the other to store all of the teams
+         */
+        team = new LinkedList<Team>();
+        teams = new LinkedList<Team>();
+    }
+
+    public void addTeam(Team aTeam) {
+        /**
+         * This method adds the information of a team to a linked list, then adds that linked list to the teams list
+         */
+        team.add(aTeam);
+        teams.addAll(team);
+    }
+    
+    public LinkedList<Team> getTeam() {
+        return team;
+    }
+    
+    public LinkedList<Team> getTeams() {
+        return teams;
+    }
+
+    public void displayTeamInfo() {
+        /**
+         * This method uses two iterators, one to iterate through the list of teams, and the other to iterate through the list of a team's information
+         * It prints out the information for each team in the teams list
+         */
+        ListIterator<Team> iterator = teams.listIterator();
+        ListIterator<Team> iter = team.listIterator();
+
+        // While loop to search through the list of teams
+        while(iterator.hasNext()) {
+            Team j = iterator.next();
+            // While loop to search through the list of information for each team and print it out
+            while(iter.hasNext()) {
+                Team l = iter.next();
+                System.out.println("Team name: " + l.getTeamName());
+                System.out.println("Coach: " + l.getCoach());
+                System.out.println("City: " + l.getCity());
+                System.out.println("State: " + l.getState());
+                System.out.println("Players: " + l.getNumOfPlayers());
+            }
+        }
+    }
+
+    public void displayTeamNames() {
+        /**
+         * This method uses two iterators, one to iterate through the list of teams, and the other to iterate through the list of a team's information
+         * It prints out all of the teams names that are stored in the teams list
+         */
+        ListIterator<Team> iterator = teams.listIterator();
+        ListIterator<Team> iter = team.listIterator();
+
+        // While loop to search through the list of teams
+        while(iterator.hasNext()) {
+            Team j = iterator.next();
+            // While loop to search through the list of information for each teams and print out only the team name
+            while(iter.hasNext()) {
+                Team l = iter.next();
+                System.out.println("Team name: " + l.getTeamName());
+            }
+        }
+    }
+}
